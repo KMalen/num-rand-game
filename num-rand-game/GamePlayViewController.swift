3eeede4fb869bed4ed3a7ba7ce4b4ec38aa4af32
Kiryl Malenchik <kirillmalen@gmail.com>
2021-03-17 16:39:44 +0300

    add connections with GameView

--- num-rand-game/GamePlayViewController.swift
+++ num-rand-game/GamePlayViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class GamePlayViewController: UIViewController {
 
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var numLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +23,10 @@ class GamePlayViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func trueDidTouch(_ sender: UIButton) {
     }
-    */
-
+    
+    @IBAction func falseDidTouch(_ sender: UIButton) {
+    }
+    
 }
