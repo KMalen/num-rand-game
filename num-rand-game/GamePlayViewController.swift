af8531048771c7b0de5311ddbf9fa390c475ad5a
Kiryl Malenchik <kirillmalen@gmail.com>
2021-03-18 22:41:37 +0300

    add expressions

--- num-rand-game/GamePlayViewController.swift
+++ num-rand-game/GamePlayViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class GamePlayViewController: UIViewController {
-
+    
     
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var numLabel: UILabel!
@@ -20,6 +20,11 @@ class GamePlayViewController: UIViewController {
     let minRand = 1
     let maxRand = 10
     
+    var firstExpr = [0,0]
+    var secondExpr = [0,0]
+    let arithmeticSigns = ["*", "-", "+", ":"]
+    var signsForExpr = 0
+    
     var timeLeft: Float = 2.0
     var givenTime: Float = 2.0
     var progress: Float = 1.0
@@ -30,24 +35,25 @@ class GamePlayViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupGame()
     }
     
-
+    
     @IBAction func trueDidTouch(_ sender: UIButton) {
         answer = true
-        checkAnswerFromUser()
+        checkAnswerFromUserExpr()
     }
     
     @IBAction func falseDidTouch(_ sender: UIButton) {
         answer = false
-        checkAnswerFromUser()
+        checkAnswerFromUserExpr()
     }
     
     func setupGame(){
         score = 0
-        resetGame()
+        //resetGame()
+        createExpr()
         gameLoop()
     }
     
@@ -72,6 +78,52 @@ class GamePlayViewController: UIViewController {
         timeLeft = givenTime
         progress = 1.0
         progressView.setProgress(progress, animated: false)
+    }
+    
+    func createExpr(){
+        for num in 0...1 {
+            firstExpr[num] = Int(arc4random_uniform(UInt32(maxRand-minRand+1))) + 1
+            secondExpr[num] = Int(arc4random_uniform(UInt32(maxRand-minRand+1))) + 1
+        }
+        
+        signsForExpr = Int(arc4random_uniform(3))
+        
+        numLabel.text = "\(firstExpr[0]) \(arithmeticSigns[signsForExpr]) \(firstExpr[1]) > \(secondExpr[0]) \(arithmeticSigns[signsForExpr]) \(secondExpr[1])"
+        
+        timeLeft = givenTime
+        progress = 1.0
+        progressView.setProgress(progress, animated: false)
+    }
+    
+    func checkAnswerFromUserExpr(){
+        var firstExprAnswer = 0
+        var secondExprAnswer = 0
+        
+        switch signsForExpr {
+        case 0:
+            firstExprAnswer = firstExpr[0] * firstExpr[1]
+            secondExprAnswer = secondExpr[0] * secondExpr[1]
+        case 1:
+            firstExprAnswer = firstExpr[0] - firstExpr[1]
+            secondExprAnswer = secondExpr[0] - secondExpr[1]
+        case 2:
+            firstExprAnswer = firstExpr[0] + firstExpr[1]
+            secondExprAnswer = secondExpr[0] + secondExpr[1]
+        case 3:
+            firstExprAnswer = firstExpr[0] / firstExpr[1]
+            secondExprAnswer = secondExpr[0] / secondExpr[1]
+        default:
+            break
+        }
+        
+        if answer == (firstExprAnswer > secondExprAnswer) {
+            score += 1
+            scoreLabel.text = "Score: \(score)"
+            createExpr()
+        } else {
+            performSegue(withIdentifier: "goToMainScreen", sender: self)
+        }
+        
     }
     
     func checkAnswerFromUser(){
