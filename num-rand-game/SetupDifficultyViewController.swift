a76715214fb133c637ea6dde78b1b7e8115cba1e
Kiryl Malenchik <kirillmalen@gmail.com>
2021-03-19 15:54:45 +0300

    add some design features to buttons

--- num-rand-game/SetupDifficultyViewController.swift
+++ num-rand-game/SetupDifficultyViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class SetupDifficultyViewController: UIViewController {
     
+    @IBOutlet weak var easyChooseButton: UIButton!
+    @IBOutlet weak var mediumChooseButton: UIButton!
+    @IBOutlet weak var hardChooseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUpButtons()
         // Do any additional setup after loading the view.
     }
     
@@ -33,6 +38,20 @@ class SetupDifficultyViewController: UIViewController {
                 break
             }
         }
+    }
+    
+    func setUpButtons(){
+        easyChooseButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        easyChooseButton.layer.backgroundColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+        easyChooseButton.layer.cornerRadius = 15
+        
+        hardChooseButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        hardChooseButton.layer.backgroundColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+        hardChooseButton.layer.cornerRadius = 15
+        
+        mediumChooseButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        mediumChooseButton.layer.backgroundColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+        mediumChooseButton.layer.cornerRadius = 15
     }
     
 }
--- num-rand-game/ViewController.swift
+++ num-rand-game/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var prevScoreLabel: UILabel!
     
     var prevScore = 0
@@ -16,11 +17,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUpButtons()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func unwindToMainScreen(segue: UIStoryboardSegue){
         prevScoreLabel.text = "Previous Score: \(prevScore)"
+    }
+    
+    func setUpButtons(){
+        startGameButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        startGameButton.layer.backgroundColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+        startGameButton.layer.cornerRadius = 15
     }
 
 }
