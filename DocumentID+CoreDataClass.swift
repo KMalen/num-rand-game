214d7ea1b01cd019b8b5d0084be71ef7117b880e
Kiryl Malenchik <kirillmalen@gmail.com>
2021-03-24 23:29:57 +0300

    add core-data models and saveContext func

---
+++ DocumentID+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DocumentID+CoreDataClass.swift
+//  num-rand-game
+//
+//  Created by Kiril Malenchik on 24.03.21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DocumentID)
+public class DocumentID: NSManagedObject {
+
+}
---
+++ GameScore+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  GameScore+CoreDataClass.swift
+//  num-rand-game
+//
+//  Created by Kiril Malenchik on 24.03.21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(GameScore)
+public class GameScore: NSManagedObject {
+
+}
